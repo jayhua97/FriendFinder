4e04d312384cd3b4ab553473a2bb0b23c7524b0c
Jayhua <jordanhua97@gmail.com>
2018-09-10 19:13:14 -0700

    added a clear to the form

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
